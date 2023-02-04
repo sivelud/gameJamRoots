8b8df9782aac978acb8d68c82b20cb89662c8297
fDukefoss <tdu009@uit.no>
2023-02-04 14:03:49 +0100

    cleaned code

--- src/classes.py
+++ src/classes.py
@@ -4,14 +4,10 @@ class Parent(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
-class Peashooter(Parent):
+class Plant(Parent):
     def __init__(self, pos):
         super().__init__()
         self.pos = pos
-        self.imgRight = pygame.image.load(peashooterRight)
-        self.imgLeft = pygame.image.load(peashooterLeft)
-        self.imgUp = pygame.image.load(peashooterUp)
-        self.imgDown = pygame.image.load(peashooterDown)
         self.imgList = [pygame.image.load(peashooterRight),pygame.image.load(peashooterDown),pygame.image.load(peashooterLeft),pygame.image.load(peashooterUp)]
         self.image = self.imgList[0]
         self.numberOfTimesClicked = 0
@@ -22,3 +18,7 @@ class Peashooter(Parent):
         self.numberOfTimesClicked +=1
         self.image = self.imgList[self.numberOfTimesClicked%4]
         self.rect = self.image.get_rect()
+
+class Peashooter(Plant):
+    def __init__(self, pos):
+        super().__init__(pos)
