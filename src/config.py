5cb57d9301fd55643885e0a02039f3be049d8d91
fDukefoss <tdu009@uit.no>
2023-02-04 12:07:56 +0100

    screen works
    works

---
+++ src/config.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame import Vector2 as v2
+import time
+
+pygame.init()
+
+"""
+* Screen
+"""
+screen_w = 950             # default=1200
+screen_h = 900              # default=700
+program_speed = 60          # default=30
+screen = pygame.display.set_mode((screen_w, screen_h))
+klokke = pygame.time.Clock()
+
+pygame.display.set_caption("Mayham game boyz")
+bg = (0, 255, 255)
+font = pygame.font.Font("freesansbold.ttf", 30)
+
+
+
+"""
+* Controls
+"""
+# Player 1
+boost_key_p1 = pygame.K_w
+left_key_p1 = pygame.K_a
+right_key_p1 = pygame.K_d
+shoot_key_p1 = pygame.K_SPACE
+# Player 2
+boost_key_p2 = pygame.K_UP
+left_key_p2 = pygame.K_LEFT
+right_key_p2 = pygame.K_RIGHT
+shoot_key_p2 = pygame.K_p
+# Both players
+reset_key = pygame.K_r
---
+++ src/engine.py
@@ -0,0 +1,35 @@
+from config import *
+
+def engine():
+
+    """
+    * Main loop
+    """
+    game_loop = True
+    running = True
+
+    while running:
+        # takes keypress input
+        for event in pygame.event.get():
+            # window X is pressed
+            if event.type == pygame.QUIT:
+                running = False
+
+        keys = pygame.key.get_pressed()
+
+        if game_loop:
+            # Fills screen with bg color
+            screen.fill(bg)
+            # Gets keypressed array
+            # Updates the players
+
+            """
+            Updates and draws the sprites from the sprite groups. 
+            """
+            
+            # Writes score and score nr
+            # Draws all the above on to the screen
+            pygame.display.flip()
+
+        # Limits program speed
+        klokke.tick(program_speed)
